060a72341f4048101a8865f6c739ca898c37431e
David Couvin <david.couvin@googlemail.com>
2020-11-11 09:33:39 -0400

    Update installer_Unix.sh

--- install/installer_Unix.sh
+++ install/installer_Unix.sh
@@ -4,11 +4,12 @@ echo 'Welcome to getSequenceInfo Unix installer !';
 echo 'cpan must first be installed on your computer';
 echo '----------------------------------------------------------';
 sudo cpan -i Date::Calc;
-sudo cpan -i Bio::SeqIO
+sudo cpan -i Bio::SeqIO;
 sudo cpan -i LWP::Simple;
 sudo cpan -i Data::Dumper;
 sudo cpan -i IO::Uncompress::Gunzip;
 sudo cpan -i IO::File;
+sudo cpan -i File::Log;
 sudo cpan -i Getopt::Long;
 sudo cpan -i Net::FTP;
 sudo cpan -i Tk;
